7332cebb61c0c4279d82515221d4a1c63773937d
trey <trey@shapeways.com>
2016-11-09 14:27:54 -0500

    setting up as repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# mckaythethird
-My new personal webcam
---
+++ js/main.js
@@ -0,0 +1,28 @@
+// FADE-OUT SUCKA
+$(document).on("click", "a", function () {
+
+    // get the href attribute
+    var newUrl = $(this).attr("href");
+
+    // veryfy if the new url exists or is a hash
+    if (!newUrl || newUrl[0] === "#") {
+        // set that hash
+        location.hash = newUrl;
+        return;
+    }
+
+    // now, fadeout the html (whole page)
+    $("html").fadeOut(300, function () {
+        // when the animation is complete, set the new location
+        location = newUrl;
+    });
+
+    // prevent the default browser behavior.
+    return false;
+});
+
+// MENU OVERLAY
+$('#toggle').click(function() {
+   $(this).toggleClass('tm-menu_container-active');
+   $('#overlay').toggleClass('open');
+  });
